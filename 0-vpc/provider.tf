edb8a4ec0eea6985994e6b659b236aff7bfeb9cc
koukuntlarakesh <koukuntlarakesh@gmail.com>
2024-03-28 14:42:02 -0500

    terraformvpc

--- 0-vpc/provider.tf
+++ 0-vpc/provider.tf
@@ -6,6 +6,8 @@ terraform {
     }
   }
   backend "s3" {
+    access_key = "AKIAX3M5KHFOAQS37E6F"
+    secret_key = "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"
     bucket         = "roboshop-dev"
     key            = "vpc-dev"
     region         = "us-east-1"
